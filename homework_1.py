0fd8dc94a40d56332a24445e85e65f22452d49bf
AndrewQuijano <andrewquijano92@gmail.com>
2018-04-04 02:43:50 -0400

    Just finished a crash course in JSON processing, pretty neat, just commented the code for myself on how to work with JSON files, FINISHED HW1

--- homework_1.py
+++
@@ -1,8 +0,0 @@
-import urllib.request
-
-# Get the content
-url = "https://api.nasa.gov/planetary/apod?api_key=Yw2oULHGrrGXP7nhbAIDonIEYuq6l5Ewns8ugb6d"
-date = "&start_date=2017-12-28"
-contents = urllib.request.urlopen(url+date).read()
-
-# Parse it to get the image URL
