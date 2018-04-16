53775fa975a182ead6221b0abb5b2b001d344ceb
Andrew Quijano <andrewquijano92@gmail.com>
2018-04-16 19:32:45 -0400

    Finished parta

--- mongodb/homework_3.py
+++ mongodb/homework_3.py
@@ -1,4 +1,5 @@
 # require the driver package
+from pprint import pprint
 import pymongo
 from pymongo import MongoClient
 
@@ -13,19 +14,25 @@ from pymongo import MongoClient
 client = MongoClient()
 
 # Get the database
-db = client.amazon
+db = client.test
 
 # Start queries
-db.find({"items.fruit": "banana"}).count()
+collection = db.movies
+movieCount = collection.find({}).count()
+print("There are " + str(movieCount) + " movies")
+
+# pprint(collection.find({"rated":"NOT RATED"})[0])
 
 # A. Update all movies with "NOT RATED" at the "rated" key to be "Pending rating". The operation must be in-place and atomic.
-db.inventory.updateMany(
-   { rated: "NOT RATED" },
-   {
-     $set: { "rated": "Pending rating" },
-     $currentDate: { lastModified: true }
-   }
+
+result = collection.update_many(
+   { "rated": "NOT RATED" }, # Given this condition being true
+   { "$set": { "rated": "Pending rating" }}
 )
+
+# print("result is: " + str(result.modified_count))
+# pprint(collection.find({"rated": "NOT RATED"}).count())
+
 # B. Find a movie with your genre in imdb and insert it into your database with the fields listed in the hw description.
 # insert it into your database with the fields listed below. Each field type must match that of the other documents in
 # the collection. For example, the genres field is an array, so your new document must also represent the genres as an array.
@@ -44,15 +51,18 @@ db.inventory.updateMany(
 # https://www.imdb.com/search/title?title_type=short&explore=genres&pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=17d3863a-3e07-4c9b-a09a-f643edc8e914&pf_rd_r=1QGYK8JNRENVXZBNN6QX&pf_rd_s=center-6&pf_rd_t=15051&pf_rd_i=genre&ref_=ft_gnr_mvpop_19
 # https://www.imdb.com/title/tt7860270/?ref_=adv_li_tt
 
-db.inventory.insertOne(
-   {
+'''
+result_b = collection.insertOne(
+{
        title: "pandas",
        year: 2018,
        counties: ["USA"],
        genres: { "Documentary", "Short" }
        directors: {"David Douglass", "Drew Fellman"}
-       #imdb{ id=1, rating=7.7, votes =38}
-    )
+       imdb{ id=1, rating=7.7, votes =38}
+)
+print("result of part b is: " + str(result_b.count))
+'''
 
 # C. Use the aggregation framework to find the total number of movies in your genre.
 
@@ -63,17 +73,23 @@ db.inventory.insertOne(
 # Select * from imdb
 # where rating = 'Short'
 # group by
-db.orders.aggregate([
+
+"""
+collection.aggregate([
     { $match: { genre: "Short" },
     { $group: { _id=>"$genre", count=>{$count: "$match"}}}}
 ])
+"""
 
 # D. Use the aggregation framework to find the number of movies made in the country you were born in with a rating of "Pending rating".
-db.orders.aggregate([
+
+"""
+collection.aggregate([
     { $match: { rating: "Pending rating", countries:"USA" }, #Check for USA?
     { $group: { _id=>{country=>"$countries", rating=>Pending rating}, count=>{$count: "$match"}}
     }}}
 ])
+"""
 
 # Example result when country is Hungary:
 #  => [{"_id"=>{"country"=>"Hungary", "rating"=>"Pending rating"}, "count"=>9}]
@@ -87,6 +103,7 @@ db.orders.aggregate([
 #    as: <output array field>
 #}
 
+"""
 $lookup:
 {
     from: hw4.pro, hw4.con
@@ -94,3 +111,5 @@ $lookup:
     foreignField: <field from the documents of the "from" collection>,
     as: <output array field>
 }
+"""
+
