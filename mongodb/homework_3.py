d439a9be71af2ee3a6b10f61af6e9f3ac4f5522e
Andrew Quijano <andrewquijano92@gmail.com>
2018-04-17 13:45:42 -0400

    Finished part E using example from mongo db. Cited it and explained in comments how it works below

--- mongodb/homework_3.py
+++ mongodb/homework_3.py
@@ -19,9 +19,8 @@ db = client.test
 # Start queries
 collection = db.movies
 movieCount = collection.find({}).count()
-print("There are " + str(movieCount) + " movies")
-
-# pprint(collection.find({"rated":"NOT RATED"})[0])
+# print("There are " + str(movieCount) + " movies")
+# print(collection.find({ })[0])
 
 # A. Update all movies with "NOT RATED" at the "rated" key to be "Pending rating". The operation must be in-place and atomic.
 
@@ -51,18 +50,16 @@ result = collection.update_many(
 # https://www.imdb.com/search/title?title_type=short&explore=genres&pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=17d3863a-3e07-4c9b-a09a-f643edc8e914&pf_rd_r=1QGYK8JNRENVXZBNN6QX&pf_rd_s=center-6&pf_rd_t=15051&pf_rd_i=genre&ref_=ft_gnr_mvpop_19
 # https://www.imdb.com/title/tt7860270/?ref_=adv_li_tt
 
-'''
-result_b = collection.insertOne(
-{
-       title: "pandas",
-       year: 2018,
-       counties: ["USA"],
-       genres: { "Documentary", "Short" }
-       directors: {"David Douglass", "Drew Fellman"}
-       imdb{ id=1, rating=7.7, votes =38}
-)
-print("result of part b is: " + str(result_b.count))
-'''
+result_b = collection.insert_one
+({
+       "title": "pandas",
+       "year": 2018,
+       "countries": ["USA"],
+       "genres": ["Documentary", "Short"],
+       "directors": ["David Douglass", "Drew Fellman"],
+       "imdb": { "id":1, "rating":7.7, "votes":38}
+})
+# print("result of part b is: " + str(result_b.modified_count))
 
 # C. Use the aggregation framework to find the total number of movies in your genre.
 
@@ -73,26 +70,37 @@ print("result of part b is: " + str(result_b.count))
 # Select * from imdb
 # where rating = 'Short'
 # group by
+# unwind, match, group then project...
 
-"""
-collection.aggregate([
-    { $match: { genre: "Short" },
-    { $group: { _id=>"$genre", count=>{$count: "$match"}}}}
+# unwind: https://docs.mongodb.com/manual/reference/operator/aggregation/unwind/
+# Count: https://docs.mongodb.com/manual/reference/operator/aggregation/count/
+'''
+resultsC = collection.aggregate
+([
+    { "$unwind": "$genres"},
+    { "$match": { "genres": "Short" }},
+    { "$group": { "_id": "$genres", "$count":"count"}},
+    { "$project": {"_id": "$genres", "$count":"count"}}
 ])
-"""
+print(resultsC.modified_count)
+'''
 
-# D. Use the aggregation framework to find the number of movies made in the country you were born in with a rating of "Pending rating".
-
-"""
-collection.aggregate([
-    { $match: { rating: "Pending rating", countries:"USA" }, #Check for USA?
-    { $group: { _id=>{country=>"$countries", rating=>Pending rating}, count=>{$count: "$match"}}
-    }}}
-])
-"""
+# D. Use the aggregation framework to find the number of movies made in the country
+# you were born in with a rating of "Pending rating".
+# unwind, match, group, project...
 
 # Example result when country is Hungary:
 #  => [{"_id"=>{"country"=>"Hungary", "rating"=>"Pending rating"}, "count"=>9}]
+
+'''
+resultsD = collection.aggregate([
+    { "$unwind": "$countries"},
+    { "$match": { "rating": "Pending rating", "countries":"USA" }},
+    { "$group": {{"_id": "$countries", "rating":"$rating"}, "$count": "count"}},
+    { "$project": {{"country": "$countries", "rating":"$rating"}, "$count": "count"}}
+])
+print(resultsD.modified_count)
+'''
 
 # E. Create an example using the $lookup pipeline operator. See hw description for more info.
 #$lookup:
@@ -102,14 +110,29 @@ collection.aggregate([
 #    foreignField: <field from the documents of the "from" collection>,
 #    as: <output array field>
 #}
+#
+# Citation:
+# https://docs.mongodb.com/manual/reference/operator/aggregation/lookup/
+# Also used the same insert command...
+#
+# db.createCollection("orders")
+# db.createCollection("inventory")
+# Used their insert command
 
-"""
-$lookup:
+db.orders.aggregate([{
+'$lookup':
 {
-    from: hw4.pro, hw4.con
-    localField: <field from the input doucments>,
-    foreignField: <field from the documents of the "from" collection>,
-    as: <output array field>
+	'from': 'inventory',
+	'localField': 'item',
+	'foreignField': 'sku',
+	'as': 'inventory_docs'
 }
-"""
+}])
+# argument explanation...
+# copy from inventory to orders
+# In orders (local to orders)
+# In inventory (local to inventory)
+# name of new document appended
+# From example, where item==sku, in orders, place the
+# matching inventory document.
 
