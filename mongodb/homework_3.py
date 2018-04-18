a03c10c4096de3d81a7213198f7c62a9acb12508
Andrew Quijano <andrewquijano92@gmail.com>
2018-04-17 22:59:19 -0400

    Done! Will try to fix it more though, but I have to finish OS HW#7 at the same time too...ugh...

--- mongodb/homework_3.py
+++ mongodb/homework_3.py
@@ -13,16 +13,18 @@ from pymongo import MongoClient
 # Create a client, this is the default
 client = MongoClient()
 
-# Get the database
+# Get the database and collection
 db = client.test
-
-# Start queries
 collection = db.movies
-movieCount = collection.find({}).count()
+
+# movieCount = collection.find({}).count()
 # print("There are " + str(movieCount) + " movies")
 # print(collection.find({ })[0])
 
-# A. Update all movies with "NOT RATED" at the "rated" key to be "Pending rating". The operation must be in-place and atomic.
+'''
+A. Update all movies with "NOT RATED" at the "rated" key to be "Pending rating". 
+The operation must be in-place and atomic.
+'''
 
 result = collection.update_many(
    { "rated": "NOT RATED" }, # Given this condition being true
@@ -32,23 +34,14 @@ result = collection.update_many(
 # print("result is: " + str(result.modified_count))
 # pprint(collection.find({"rated": "NOT RATED"}).count())
 
-# B. Find a movie with your genre in imdb and insert it into your database with the fields listed in the hw description.
-# insert it into your database with the fields listed below. Each field type must match that of the other documents in
-# the collection. For example, the genres field is an array, so your new document must also represent the genres as an array.
-
-# title (string)
-# year (integer)
-# countries (array)
-# genres (array)
-# directors (array)
-# imdb (document with fields: 'id' (integer),
-# 'rating' (float),
-# 'votes' (integer).
-# If the values aren't available for the movie you chose, find one that has those values or make the values up)
-
-# My Link:
-# https://www.imdb.com/search/title?title_type=short&explore=genres&pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=17d3863a-3e07-4c9b-a09a-f643edc8e914&pf_rd_r=1QGYK8JNRENVXZBNN6QX&pf_rd_s=center-6&pf_rd_t=15051&pf_rd_i=genre&ref_=ft_gnr_mvpop_19
-# https://www.imdb.com/title/tt7860270/?ref_=adv_li_tt
+'''
+B. Find a movie with your genre in imdb and insert it into your database with the fields listed in the hw description.
+insert it into your database with the fields listed below. Each field type must match that of the other documents in
+the collection. For example, the genres field is an array, so your new document must also represent the genres as an array.
+My Link:
+https://www.imdb.com/search/title?title_type=short&explore=genres&pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=17d3863a-3e07-4c9b-a09a-f643edc8e914&pf_rd_r=1QGYK8JNRENVXZBNN6QX&pf_rd_s=center-6&pf_rd_t=15051&pf_rd_i=genre&ref_=ft_gnr_mvpop_19
+https://www.imdb.com/title/tt7860270/?ref_=adv_li_tt
+'''
 
 result_b = collection.insert_one
 ({
@@ -59,80 +52,84 @@ result_b = collection.insert_one
        "directors": ["David Douglass", "Drew Fellman"],
        "imdb": { "id":1, "rating":7.7, "votes":38}
 })
-# print("result of part b is: " + str(result_b.modified_count))
+# print("result of part b is: " + list(result_b))
 
-# C. Use the aggregation framework to find the total number of movies in your genre.
-
-# Example result:
-#  => [{"_id"=>"Comedy", "count"=>14046}]
-
-# SQL:
-# Select * from imdb
-# where rating = 'Short'
-# group by
-# unwind, match, group then project...
-
-# unwind: https://docs.mongodb.com/manual/reference/operator/aggregation/unwind/
-# Count: https://docs.mongodb.com/manual/reference/operator/aggregation/count/
 '''
+C. Use the aggregation framework to find the total number of movies in your genre.
+Example result:
+=> [{"_id"=>"Comedy", "count"=>14046}]
+unwind: https://docs.mongodb.com/manual/reference/operator/aggregation/unwind/
+Count: https://docs.mongodb.com/manual/reference/operator/aggregation/count/
+Another way to count documents in group:
+https://stackoverflow.com/questions/40791907/what-does-sum1-mean-in-mongo
+'''
+
 resultsC = collection.aggregate
 ([
     { "$unwind": "$genres"},
     { "$match": { "genres": "Short" }},
-    { "$group": { "_id": "$genres", "$count":"count"}},
-    { "$project": {"_id": "$genres", "$count":"count"}}
+    { "$group": { "_id": "=>Short", "count":{"$sum":1}}}
 ])
-print(resultsC.modified_count)
+print(type(resultsC))
+#print(resultsC.find({ }))
+'''
+D. Use the aggregation framework to find the number of movies made in the country
+you were born in with a rating of "Pending rating".
+unwind, match, group, project.
+
+Example result when country is Hungary:
+=> [{"_id"=>{"country"=>"Hungary", "rating"=>"Pending rating"}, "count"=>9}]
 '''
 
-# D. Use the aggregation framework to find the number of movies made in the country
-# you were born in with a rating of "Pending rating".
-# unwind, match, group, project...
-
-# Example result when country is Hungary:
-#  => [{"_id"=>{"country"=>"Hungary", "rating"=>"Pending rating"}, "count"=>9}]
-
-'''
 resultsD = collection.aggregate([
     { "$unwind": "$countries"},
     { "$match": { "rating": "Pending rating", "countries":"USA" }},
-    { "$group": {{"_id": "$countries", "rating":"$rating"}, "$count": "count"}},
-    { "$project": {{"country": "$countries", "rating":"$rating"}, "$count": "count"}}
+    { "$group": {"_id": "country=>USA, rating=>Pending rating", "count": {"$sum":1}}}
 ])
-print(resultsD.modified_count)
-'''
+# print(list(resultsD))
 
-# E. Create an example using the $lookup pipeline operator. See hw description for more info.
-#$lookup:
-#{
-#    from: <collections to join>,
-#    localField: <field from the input doucments>,
-#    foreignField: <field from the documents of the "from" collection>,
-#    as: <output array field>
-#}
-#
-# Citation:
-# https://docs.mongodb.com/manual/reference/operator/aggregation/lookup/
-# Also used the same insert command...
-#
+# { "$project": {{"country": "$countries", "rating":"$rating"}, "$count": "count"}}
+
+
+'''
+E. Create an example using the $lookup pipeline operator. See hw description for more info.
+$lookup:
+{
+    from: <collections to join>,
+    localField: <field from the input doucments>,
+    foreignField: <field from the documents of the "from" collection>,
+    as: <output array field>
+}
+Citation:
+https://docs.mongodb.com/manual/reference/operator/aggregation/lookup/
+'''
+# To create new collections, I ran this on the shell...
 # db.createCollection("orders")
 # db.createCollection("inventory")
-# Used their insert command
+
+# Clean everything...
+db.orders.remove( { } )
+db.inventory.remove( { } )
+
+# Now insert!
+db.orders.insert([
+   { "_id" : 3, "item" : "almonds", "price" : 12, "quantity" : 2 },
+   { "_id" : 4, "item" : "pecans", "price" : 20, "quantity" : 1 }
+])
+
+db.inventory.insert([
+   { "_id" : 5, "sku" : "almonds", "description": "product 1", "instock" : 120 },
+   { "_id" : 6, "sku" : "bread", "description": "product 2", "instock" : 80 },
+   { "_id" : 7, "sku" : "cashews", "description": "product 3", "instock" : 60 },
+   { "_id" : 8, "sku" : "pecans", "description": "product 4", "instock" : 70 },
+])
 
 db.orders.aggregate([{
-'$lookup':
+"$lookup":
 {
-	'from': 'inventory',
-	'localField': 'item',
-	'foreignField': 'sku',
-	'as': 'inventory_docs'
+    'from': "inventory",
+    'localField': "item",
+    'foreignField': "sku",
+    'as': "inventory_docs"
 }
 }])
-# argument explanation...
-# copy from inventory to orders
-# In orders (local to orders)
-# In inventory (local to inventory)
-# name of new document appended
-# From example, where item==sku, in orders, place the
-# matching inventory document.
-
